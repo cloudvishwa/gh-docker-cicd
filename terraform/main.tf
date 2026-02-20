993d8734d55ab5761fc26f0d6bbd8e948d28df1a
vishwa <vishwa@g.com>
2026-02-20 17:29:20 +0530

    commented profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,9 @@
 # Specifies which cloud provider to use and in which region
 provider "aws" {
   region = "us-west-1"
-  profile = "default"
+  #profile = "default"
 }
 
-# # EC2 Instance Resource
-# # Creates a single EC2 instance in AWS
-# resource "aws_instance" "vm01" {
-
-#   # AMI (Amazon Machine Image) - Amazon Linux 2023 for us-east-1
-#   ami = "ami-0e2de80e7636c4837"
-#   # Challenge: use latest LTS AMI id from Amazon using data sources (data-ami.tf)
-#   # ami = data.aws_ami.amazon_linux_2023.id
-
-#   instance_type = "t3.micro"
-
-#   tags = {
-#     Name = "Vishwa"
-#   }
-# }
 
 # Latest Ubuntu 22.04 LTS AMI
 data "aws_ami" "ubuntu" {
